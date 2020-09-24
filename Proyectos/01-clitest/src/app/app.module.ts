9fab9f19993edb12c70193b5bb14ed2318d691ea
Bryan linux-mint <solares.josue@outlook.com>
2020-09-24 14:32:23 -0600

    Se avanza hasta sección 3.14

--- Proyectos/01-clitest/src/app/app.module.ts
+++ Proyectos/01-clitest/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
