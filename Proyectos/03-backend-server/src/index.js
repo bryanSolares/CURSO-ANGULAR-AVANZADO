b72e6acb80975ca9530cc699193d70598a4b3c86
Bryan-kubuntu <solares.josue@outlook.com>
2020-09-30 20:27:06 -0600

    Fin sección 10 CRUD y TOKEN

--- Proyectos/03-backend-server/src/index.js
+++ Proyectos/03-backend-server/src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const routes = require("./routes/index");
+const routerMain = require("./routes/index");
 require("dotenv").config();
 require("./database/config");
 
 app.use(cors());
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routerMain);
+app.use("/api/usuarios", require("./routes/usuario.routes"));
+app.use("/api/auth", require("./routes/auth.routes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Aplicación en puerto ${process.env.PORT}`);
